dd91e2d17d18cb447befda376ef66d07d6e50318
ozankersit <ozankersit@gmail.com>
2022-12-13 20:48:16 +0300

    statistics section updates

--- components/ui/StatisticsSection.tsx
+++ components/ui/StatisticsSection.tsx
@@ -4,12 +4,12 @@ export const StatisticsSection:FC = () => {
   return (
     <>
    <div className="bg-[url('/ryzeBg.png')] w-full h-[620px] flex justify-center items-center bg-center">
-        <div className='flex flex-wrap justify-center md:flex-row flex-col gap-36'>
+        <div className='flex flex-wrap justify-center md:flex-row flex-col'>
             <div className='flex flex-col'>
                 <span className='text-white text-3xl-bold'>54,645</span>
                 <span className='text-white text-3xl-bold'>Completed Orders</span>
             </div>
-            <div className='flex flex-col'>
+            <div className='flex flex-col md:mx-36 mx-auto md:my-auto my-36'>
                 <span className='text-white text-3xl-bold'>29,380</span>
                 <span className='text-white text-3xl-bold'>Happy Customers</span>
             </div>
