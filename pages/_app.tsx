82f662ce999b6b398a33ae3da01644ea0cae35fc
ozankersit <ozankersit@gmail.com>
2022-12-13 20:36:28 +0300

    build error fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Layout from "../components/Layout/Layout";
+import Layout from '../components/layout/Layout'
+
 
 
 function MyApp({ Component, pageProps }: AppProps) {
